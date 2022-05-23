111c9c18090b57ee73549ff3c47cf3b52cd9a899
Parimal Yeole <parimal.yeole@moduscreate.com>
2022-05-23 11:50:42 +0530

    🙈  added git ignore file

--- src/database.ts
+++ src/database.ts
@@ -1,9 +1,23 @@
 import sqlite3 from "sqlite3";
 import { open } from "sqlite";
+import path from 'path';
 
 export const initDatabase = () => {
   return open({
-    filename: "/sandbox/database.db",
+    filename: path.resolve(__dirname, '../database.db'),
     driver: sqlite3.Database
   });
 };
+
+
+// initDatabase().then(async (_db) => {
+//   console.log("Database initialized");
+// //  const dbdd =  await _db.all("select name from sqlite_master where type='table'");
+// try {
+//   const aaa = await _db.all('SELECT * FROM artists');
+//   console.log('result ====>', JSON.stringify(aaa,null,2));
+// } catch (error) {
+//   console.log('error ====>', error);
+// }
+  
+// });
