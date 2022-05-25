7bdc80ee75019dc99ef57104e1d1073239b30788
Parimal Yeole <parimal.yeole@moduscreate.com>
2022-05-25 11:26:33 +0530

    Added sorting pagination and filter on artist list

--- src/schema.ts
+++ src/schema.ts
@@ -1,7 +1,7 @@
 import "graphql-import-node";
 import { makeExecutableSchema } from "@graphql-tools/schema";
 import typeDefs from "./schema.graphql";
-import { Artists } from "@prisma/client";
+import { Artists, Prisma } from "@prisma/client";
 import { GraphQLContext } from "./context";
 
 const resolvers = {
@@ -14,8 +14,36 @@ const resolvers = {
         .albums(),
   },
   Query: {
-    artists: async (_parent: unknown, _args: {}, context: GraphQLContext) => {
-      return context.prisma.artists.findMany();
+    artistList: async (
+      _parent: unknown,
+      args: {
+        filter?: string;
+        skip?: number;
+        take?: number;
+        orderBy?: {
+          name?: Prisma.SortOrder;
+        };
+      },
+      context: GraphQLContext
+    ) => {
+      const where = args.filter
+      ? {
+          OR: [
+            { name: { contains: args.filter } },
+          ],
+        }
+      : {};
+    const totalCount = await context.prisma.artists.count({ where });
+    const artists = await context.prisma.artists.findMany({
+      where,
+      skip: args.skip || 0,
+      take: args.take || 20,
+      orderBy: args.orderBy,
+    });
+    return {
+      count: totalCount,
+      artists,
+    }
     },
   },
   Mutation: {
